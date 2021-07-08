5f5182de8484490b226560d07c235c4a2d3c9415
Frederico Neres <neresfrederico@gmail.com>
2021-07-08 11:56:48 -0300

    Healthcheck GRPC

---
+++ src/main/kotlin/br/com/zup/HealthCheckService.kt
@@ -0,0 +1,34 @@
+package br.com.zup
+
+import grpc.health.v1.HealthGrpc
+import grpc.health.v1.HealthOuterClass
+import grpc.health.v1.HealthOuterClass.HealthCheckResponse
+import io.grpc.stub.StreamObserver
+import javax.inject.Singleton
+
+@Singleton
+class HealthCheckService: HealthGrpc.HealthImplBase() {
+
+    override fun check(
+        request: HealthOuterClass.HealthCheckRequest?,
+        responseObserver: StreamObserver<HealthCheckResponse>?
+    ) {
+
+        responseObserver?.onNext(HealthCheckResponse.newBuilder()
+            .setStatus(HealthCheckResponse.ServingStatus.SERVING)
+            .build())
+
+        responseObserver?.onCompleted()
+    }
+
+    override fun watch(
+        request: HealthOuterClass.HealthCheckRequest?,
+        responseObserver: StreamObserver<HealthCheckResponse>?
+    ) {
+        responseObserver?.onNext(HealthCheckResponse.newBuilder()
+            .setStatus(HealthCheckResponse.ServingStatus.SERVING)
+            .build())
+
+        responseObserver?.onCompleted()
+    }
+}
